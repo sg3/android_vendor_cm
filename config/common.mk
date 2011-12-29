0fe16d02f2c0d83b4752683a9904be5c87fa9aa0
Yoann Laissus <yoann.laissus@gmail.com>
2011-12-29 11:04:03 +0100

    Remove a library
    Change-Id: I44f7c9b8b000cfdb6b8baea2fd1a6e151cfcd5c5

--- config/common.mk
+++ config/common.mk
@@ -41,7 +41,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES +=  \
     vendor/cm/proprietary/RomManager.apk:system/app/RomManager.apk \
     vendor/cm/proprietary/Term.apk:system/app/Term.apk \
-    vendor/cm/proprietary/lib/armeabi/libjackpal-androidterm3.so:system/lib/libjackpal-androidterm3.so \
     vendor/cm/prebuilt/common/bootanimation.zip:system/media/bootanimation.zip
   	
 
